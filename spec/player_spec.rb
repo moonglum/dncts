6bfa93063e1d2b4f74557eaa4eab2a8293def965
moonglum <moonglum@moonbeamlabs.com>
2013-06-24 23:16:44 +0200

    Extracted the knowledge about the Player class from Handler

---
+++ spec/player_spec.rb
@@ -0,0 +1,16 @@
+require "./spec/spec_helper"
+require "./app/player"
+
+describe Player do
+  describe "class methods" do
+    describe "[]"
+    describe "create"
+    describe ""
+  end
+
+  describe "instance methods" do
+    describe "update"
+    describe "set_statistics"
+    describe "id"
+  end
+end
