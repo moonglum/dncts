664de868ba3c48d597b0a35de935620ddf6e29e7
moonglum <moonglum@moonbeamlabs.com>
2013-06-24 11:42:44 +0200

    Refactored: HandlerSpec

--- spec/handler_spec.rb
+++ spec/handler_spec.rb
@@ -7,18 +7,23 @@ describe Handler do
   subject { Handler.new(player_class, lobby_class) }
 
   describe "interaction with player class" do
-    describe "update_position" do
-      let(:player) { double }
-      let(:player_id) { 1 }
-      let(:lat) { "50.941448" }
-      let(:lon) { "6.958373" }
+    let(:player) { double }
+    let(:player_id) { 1 }
+    let(:player_lat) { "50.941448" }
+    let(:player_lon) { "6.958373" }
+    let(:player_id) { 1 }
+    let(:player_statistics) { double }
 
+    before {
+      allow(player_class).to receive(:[]).with(player_id).and_return {
+        player
+      }
+    }
+
+    describe "update_position" do
       it "should update the position of an existing player" do
-        allow(player_class).to receive(:[]).with(player_id).and_return {
-          player
-        }
-        expect(player).to receive(:update).with(:lat => lat, :lon => lon)
-        subject.update_position(player_id, lat, lon)
+        expect(player).to receive(:update).with(:lat => player_lat, :lon => player_lon)
+        subject.update_position(player_id, player_lat, player_lon)
       end
 
       it "should raise an exception if the player was not found" do
@@ -26,7 +31,7 @@ describe Handler do
           nil
         }
         expect {
-          subject.update_position(player_id, lat, lon)
+          subject.update_position(player_id, player_lat, player_lon)
         }.to raise_exception(ApiError, "Player not found")
       end
 
@@ -34,19 +39,12 @@ describe Handler do
         allow(player_class).to receive(:[]).with(player_id).and_return {
           double.as_null_object
         }
-        expect(subject.update_position(player_id, lat, lon)).to eq(subject)
+        expect(subject.update_position(player_id, player_lat, player_lon)).to eq(subject)
       end
     end
 
     describe "set_player_statistics" do
-      let(:player) { double }
-      let(:player_id) { 1 }
-      let(:player_statistics) { double }
-
       it "should set the player statistics for an existing player" do
-        allow(player_class).to receive(:[]).with(player_id).and_return {
-          player
-        }
         expect(player).to receive(:set_statistics).with(player_statistics)
         subject.set_player_statistics(player_id, player_statistics)
       end
@@ -70,20 +68,24 @@ describe Handler do
   end
 
   describe "interaction with lobby class" do
+    let(:lobby) { double }
+    let(:lobby_id) { 1 }
+
+    before do
+      allow(lobby_class).to receive(:[]).with(lobby_id).and_return {  
+        lobby
+      }
+      allow(lobby).to receive(:started?).and_return {
+        true
+      }
+    end
+
     describe "get_current_game_state" do
-      let(:lobby) { double }
-      let(:lobby_id) { 1 }
       let(:game_state) { double }
 
       before do
-        allow(lobby_class).to receive(:[]).with(lobby_id).and_return {  
-          lobby
-        }
         allow(lobby).to receive(:game_state).and_return {
           game_state
-        }
-        allow(lobby).to receive(:started?).and_return {
-          true
         }
       end
 
@@ -102,15 +104,6 @@ describe Handler do
     end
 
     describe "finish_game" do
-      let(:lobby) { double }
-      let(:lobby_id) { 1 }
-
-      before do
-        allow(lobby_class).to receive(:[]).with(lobby_id).and_return {  
-          lobby
-        }
-      end
-
       it "should finish an existing game" do
         expect(lobby).to receive(:finish_game)
         subject.finish_game(lobby_id)
@@ -132,20 +125,15 @@ describe Handler do
     end
 
     describe "get_game_statistics" do
-      let(:lobby) { double }
-      let(:lobby_id) { 1 }
       let(:game_statistics) { double }
 
-      before do
-        allow(lobby_class).to receive(:[]).with(lobby_id).and_return {  
-          lobby
-        }
-      end
-
-      it "should return the game statistics for an existing lobby" do
+      before {
         allow(lobby).to receive(:game_statistics).and_return {  
           game_statistics
         }
+      }
+
+      it "should return the game statistics for an existing lobby" do
         expect(subject.get_game_statistics(lobby_id)).to eq(game_statistics)
       end
 
@@ -159,5 +147,4 @@ describe Handler do
       end
     end
   end
-
 end
