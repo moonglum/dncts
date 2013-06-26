11c707a6228c906e1d7c59dbd419fb4b6b03fc92
moonglum <moonglum@moonbeamlabs.com>
2013-06-26 21:42:24 +0200

    Extracted the interface of the Vertex class

---
+++ spec/vertex_spec.rb
@@ -0,0 +1,16 @@
+require "./spec/spec_helper"
+require "./app/vertex"
+
+describe Vertex do
+  describe "class methods" do
+    describe "[]"
+    describe "create"
+  end
+
+  describe "instance methods" do
+    describe "update"
+    describe "lat"
+    describe "lon"
+    describe "carrier"
+  end
+end
