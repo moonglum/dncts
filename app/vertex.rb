8131d2681cb355076661b940473ea910a626a48c
moonglum <moonglum@moonbeamlabs.com>
2013-06-26 23:00:00 +0200

    Implemented: Vertex Class

--- app/vertex.rb
+++ app/vertex.rb
@@ -1,2 +1,9 @@
-class Vertex
+require "ohm"
+require "json"
+
+class Vertex < Ohm::Model
+  attribute :lat
+  attribute :lon
+  attribute :portable, lambda { |raw| raw == "true" || raw == true }
+  attribute :carrier, lambda { |raw| raw.to_i }
 end
--- spec/vertex_spec.rb
+++ spec/vertex_spec.rb
@@ -2,9 +2,26 @@ require "./spec/spec_helper"
 require "./app/vertex"
 
 describe Vertex do
-  describe "class methods" do
-    describe "[]"
-    describe "create"
+  before(:all) {
+    Ohm.connect
+  }
+
+  describe "Needed Ohm functionality" do
+    let(:lat) { "50.941394" }
+    let(:lon) { "6.958416" }
+    let(:portable) { true }
+    let(:carrier) { 5 }
+
+    it "should create, update and find a vertex and support all neccessary getters" do
+      old_vertex = Vertex.create(:lat => lat, :lon => lon, :portable => portable)
+      old_vertex.update(:carrier => 5)
+      vertex = Vertex[old_vertex.id]
+      expect(old_vertex).to eq(vertex)
+      expect(vertex.lat).to eq(lat)
+      expect(vertex.lon).to eq(lon)
+      expect(vertex.portable).to eq(portable)
+      expect(vertex.carrier).to eq(carrier)
+    end
   end
 
   describe "instance methods" do
