e49e22287589d69cf5cacd4991d3f3c64f4f7949
moonglum <moonglum@moonbeamlabs.com>
2013-06-26 22:24:41 +0200

    Player#update_statistics and Player#statistics

--- app/player.rb
+++ app/player.rb
@@ -1,5 +1,16 @@
+require "json"
+
 class Player < Ohm::Model
   attribute :name
   attribute :lat
   attribute :lon
+  attribute :raw_statistics
+
+  def update_statistics(statistics)
+    update :raw_statistics => JSON.dump(statistics)
+  end
+
+  def statistics
+    JSON.parse get(:raw_statistics)
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -14,9 +14,10 @@ describe Player do
     let(:lon) { "6.958416" }
 
     it "should create, update and find a player and support all neccessary getters" do
-      player = Player.create(:name => name, :lat => lat, :lon => lon)
-      player.update(:name => new_name)
-      expect(Player[player.id]).to eq(player)
+      old_player = Player.create(:name => name, :lat => lat, :lon => lon)
+      old_player.update(:name => new_name)
+      player = Player[old_player.id]
+      expect(old_player).to eq(player)
       expect(player.name).to eq(new_name)
       expect(player.lat).to eq(lat)
       expect(player.lon).to eq(lon)
@@ -24,6 +25,18 @@ describe Player do
   end
 
   describe "instance methods" do
-    describe "set_statistics"
+    describe "set_statistics" do
+      let(:statistics) {{
+        "distance" => 41,
+        "different_vertices_touched" => 12,
+        "total_vertices_touched" => 31
+      }}
+
+      it "should get the statistics back as they were stored" do
+        player = Player.create
+        player.update_statistics statistics
+        expect(player.statistics).to eq(statistics)
+      end
+    end
   end
 end
