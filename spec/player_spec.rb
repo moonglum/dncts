8e714c0c00ff8f5887fa8a9a31c1eb7bd3268ab7
moonglum <moonglum@moonbeamlabs.com>
2013-06-26 23:13:47 +0200

    Streamlining Player and Vertex Spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,36 +6,24 @@ describe Player do
     Ohm.connect
   }
 
-  describe "Needed Ohm functionality" do
-    let(:name) { "moonglum" }
-    let(:new_name) { "_moonglum" }
-    let(:lat) { "50.941394" }
-    let(:lon) { "6.958416" }
+  let(:name) { "moonglum" }
+  let(:new_name) { "_moonglum" }
+  let(:lat) { "50.941394" }
+  let(:lon) { "6.958416" }
+  let(:statistics) {{
+    "distance" => 41,
+    "different_vertices_touched" => 12,
+    "total_vertices_touched" => 31
+  }}
 
-    it "should create, update and find a player and support all neccessary getters" do
-      old_player = Player.create(:name => name, :lat => lat, :lon => lon)
-      old_player.update(:name => new_name)
-      player = Player[old_player.id]
-      expect(old_player).to eq(player)
-      expect(player.name).to eq(new_name)
-      expect(player.lat).to eq(lat)
-      expect(player.lon).to eq(lon)
-    end
-  end
-
-  describe "instance methods" do
-    describe "set_statistics" do
-      let(:statistics) {{
-        "distance" => 41,
-        "different_vertices_touched" => 12,
-        "total_vertices_touched" => 31
-      }}
-
-      it "should get the statistics back as they were stored" do
-        player = Player.create
-        player.update :statistics => statistics
-        expect(player.statistics).to eq(statistics)
-      end
-    end
+  it "should create, update and find a player and support all neccessary getters" do
+    old_player = Player.create(:name => name, :lat => lat, :lon => lon)
+    old_player.update(:name => new_name, :statistics => statistics)
+    player = Player[old_player.id]
+    expect(old_player).to eq(player)
+    expect(player.name).to eq(new_name)
+    expect(player.lat).to eq(lat)
+    expect(player.lon).to eq(lon)
+    expect(player.statistics).to eq(statistics)
   end
 end
--- spec/vertex_spec.rb
+++ spec/vertex_spec.rb
@@ -6,28 +6,19 @@ describe Vertex do
     Ohm.connect
   }
 
-  describe "Needed Ohm functionality" do
-    let(:lat) { "50.941394" }
-    let(:lon) { "6.958416" }
-    let(:portable) { true }
-    let(:carrier) { 5 }
+  let(:lat) { "50.941394" }
+  let(:lon) { "6.958416" }
+  let(:portable) { true }
+  let(:carrier) { 5 }
 
-    it "should create, update and find a vertex and support all neccessary getters" do
-      old_vertex = Vertex.create(:lat => lat, :lon => lon, :portable => portable)
-      old_vertex.update(:carrier => 5)
-      vertex = Vertex[old_vertex.id]
-      expect(old_vertex).to eq(vertex)
-      expect(vertex.lat).to eq(lat)
-      expect(vertex.lon).to eq(lon)
-      expect(vertex.portable).to eq(portable)
-      expect(vertex.carrier).to eq(carrier)
-    end
-  end
-
-  describe "instance methods" do
-    describe "update"
-    describe "lat"
-    describe "lon"
-    describe "carrier"
+  it "should create, update and find a vertex and support all neccessary getters" do
+    old_vertex = Vertex.create(:lat => lat, :lon => lon, :portable => portable)
+    old_vertex.update(:carrier => 5)
+    vertex = Vertex[old_vertex.id]
+    expect(old_vertex).to eq(vertex)
+    expect(vertex.lat).to eq(lat)
+    expect(vertex.lon).to eq(lon)
+    expect(vertex.portable).to eq(portable)
+    expect(vertex.carrier).to eq(carrier)
   end
 end
