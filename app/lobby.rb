fcb1f3ac2e8c65bd5c35a7b011df3d70f970cd89
moonglum <moonglum@moonbeamlabs.com>
2013-06-27 09:05:22 +0200

    Lobby: Implemented Basics / Ohm Integration

--- app/lobby.rb
+++ app/lobby.rb
@@ -1,2 +1,9 @@
-class Lobby
+require "ohm"
+require "ohm/contrib"
+
+class Lobby < Ohm::Model
+  include Ohm::DataTypes
+
+  attribute :name
+  attribute :edges, Type::Array
 end
--- spec/lobby_spec.rb
+++ spec/lobby_spec.rb
@@ -2,23 +2,35 @@ require "./spec/spec_helper"
 require "./app/lobby"
 
 describe Lobby do
-  describe "class methods" do
-    describe "[]" # with(lobby_id)
-    describe "create" # with(lobby_name)
+  before(:all) {
+    Ohm.connect
+  }
+
+  let(:name) { "The Game" }
+  let(:new_name) { "Nice Game" }
+  let(:edges) { [ { "vertex_a_id" => 12, "vertex_b_id" => 1, "color" => "red"  } ] }
+
+  it "should create, update and find a lobby and support all neccessary getters" do
+    old_lobby = Lobby.create(:name => name)
+    old_lobby.update(:name => new_name, :edges => edges)
+    lobby = Lobby[old_lobby.id]
+    expect(old_lobby).to eq(lobby)
+    expect(lobby.name).to eq(new_name)
+    expect(lobby.edges).to eq(edges)
   end
 
   describe "instance methods" do
-    describe "started?"
     describe "game_state"
-    describe "finish_game"
     describe "game_statistics"
     describe "game"
-    describe "id"
-    describe "lobby_name"
+
     describe "vertices"
-    describe "edges"
+
     describe "add_player" # with(player)
-    describe "start_game" # with(graph)
     describe "remove_player" # with(player)
+
+    describe "start_game" # with(graph)
+    describe "started?"
+    describe "finish_game"
   end
 end
